7097a6cb4d8b90086220ef526fd3bafb5fb86683
CodeBuzzers <codebuzzers@CodeBuzzerss-MacBook-Pro.local>
2021-03-01 15:14:19 +0530

    first  commit

---
+++ MYTSpod/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MYTSpod
+//
+//  Created by CodeBuzzers on 01/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
